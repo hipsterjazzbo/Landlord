d9443e99bf75afbd7b62568346f3a492228f4d1d
James Brooks <james@bluebaytravel.co.uk>
2015-01-26 15:38:50 +0000

    Code style fixes.

--- src/AuraIsHere/LaravelMultiTenant/LaravelMultiTenantServiceProvider.php
+++ src/AuraIsHere/LaravelMultiTenant/LaravelMultiTenantServiceProvider.php
@@ -3,56 +3,54 @@
 use Illuminate\Foundation\AliasLoader;
 use Illuminate\Support\ServiceProvider;
 
-class LaravelMultiTenantServiceProvider extends ServiceProvider {
+class LaravelMultiTenantServiceProvider extends ServiceProvider
+{
+    /**
+     * Indicates if loading of the provider is deferred.
+     *
+     * @var bool
+     */
+    protected $defer = false;
 
-	/**
-	 * Indicates if loading of the provider is deferred.
-	 *
-	 * @var bool
-	 */
-	protected $defer = false;
+    /**
+     * Bootstrap the application events.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        $this->package('aura-is-here/laravel-multi-tenant');
+    }
 
-	/**
-	 * Bootstrap the application events.
-	 *
-	 * @return void
-	 */
-	public function boot()
-	{
-		$this->package('aura-is-here/laravel-multi-tenant');
-	}
+    /**
+     * Register the service provider.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        // Register our tenant scope instance
+        $this->app->bindshared('AuraIsHere\LaravelMultiTenant\TenantScope', function ($app) {
+            return new TenantScope();
+        });
 
-	/**
-	 * Register the service provider.
-	 *
-	 * @return void
-	 */
-	public function register()
-	{
-		// Register our tenant scope instance
-		$this->app->bindshared('AuraIsHere\LaravelMultiTenant\TenantScope', function ($app)
-		{
-			return new TenantScope();
-		});
+        // Define alias 'TenantScope'
+        $this->app->booting(function () {
+            $loader = AliasLoader::getInstance();
+            $loader->alias('TenantScope', 'AuraIsHere\LaravelMultiTenant\Facades\TenantScopeFacade');
+        });
 
-		// Define alias 'TenantScope'
-		$this->app->booting(function ()
-		{
-			$loader = AliasLoader::getInstance();
-			$loader->alias('TenantScope', 'AuraIsHere\LaravelMultiTenant\Facades\TenantScopeFacade');
-		});
+        // Register our config
+        $this->app['config']->package('aura-is-here/laravel-multi-tenant', __DIR__.'/../../config');
+    }
 
-		// Register our config
-		$this->app['config']->package('aura-is-here/laravel-multi-tenant', __DIR__ . '/../../config');
-	}
-
-	/**
-	 * Get the services provided by the provider.
-	 *
-	 * @return array
-	 */
-	public function provides()
-	{
-		return array();
-	}
+    /**
+     * Get the services provided by the provider.
+     *
+     * @return array
+     */
+    public function provides()
+    {
+        return [];
+    }
 }
