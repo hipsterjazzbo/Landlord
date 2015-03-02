1c6a267b32ddde2246d97c64f159d23a26b870be
Alan Rezende <ar@arcriacoes.com.br>
2015-03-02 09:55:56 -0300

    Update TenantScopedModelTrait.php

--- src/AuraIsHere/LaravelMultiTenant/Traits/TenantScopedModelTrait.php
+++ src/AuraIsHere/LaravelMultiTenant/Traits/TenantScopedModelTrait.php
@@ -77,7 +77,7 @@ trait TenantScopedModelTrait
     public static function findOrFail($id, $columns = ['*'])
     {
         try {
-            return call_user_func_array([static::query(), 'findOrFail'], [$id, $columns]);
+            return parent::query()->findOrFail($id, $columns);
         } catch (ModelNotFoundException $e) {
             throw with(new TenantModelNotFoundException())->setModel(get_called_class());
         }
