a7f42505ec6ab35258ba6b0063a633fe71580e1e
Alan Rezende <ar@arcriacoes.com.br>
2015-03-04 03:12:47 -0300

    Update parent to static

--- src/AuraIsHere/LaravelMultiTenant/Traits/TenantScopedModelTrait.php
+++ src/AuraIsHere/LaravelMultiTenant/Traits/TenantScopedModelTrait.php
@@ -77,7 +77,7 @@ trait TenantScopedModelTrait
     public static function findOrFail($id, $columns = ['*'])
     {
         try {
-            return parent::query()->findOrFail($id, $columns);
+            return static::query()->findOrFail($id, $columns);
         } catch (ModelNotFoundException $e) {
             throw with(new TenantModelNotFoundException())->setModel(get_called_class());
         }
