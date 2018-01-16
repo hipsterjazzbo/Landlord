350b73a7cfa3c5b95603c42001b5f5b5c7fba044
James Brooks <james@alt-three.com>
2018-01-16 17:06:12 +0000

    Add nullable check for non-scoped data

--- src/TenantManager.php
+++ src/TenantManager.php
@@ -162,7 +162,7 @@ class TenantManager
                     $id = $this->getTenants()->first();
                 }
 
-                $builder->where($model->getQualifiedTenant($tenant), '=', $id);
+                $builder->where($model->getQualifiedTenant($tenant), '=', $id)>orWhereNull($model->getQualifiedTenant($tenant));
             });
         });
     }
